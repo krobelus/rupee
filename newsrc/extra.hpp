fbdce5dca8ea7de36db167dfea253d238d78aa8d
Adrian Rebola-Pardo <arpj.rebola@gmail.com>
2017-02-10 15:02:12 +0100

    newsrc Database

---
+++ newsrc/extra.hpp
@@ -0,0 +1,78 @@
+#ifndef __EXTRA_H_INCLUDED__
+#define __EXTRA_H_INCLUDED__
+
+#include <string>
+
+namespace Constants {
+	const int ExtraCellsDatabase = 1;
+	const int FlagsCellDatabase = -1;
+	const int ActivityBit = 0;
+	const int VerificationBit = 1;
+	const int OriginalityBit = 2;
+	const int PersistencyBit = 3;
+    const int PseudounitBit = 4;
+    const bool ActiveFlag = true;
+    const bool InactiveFlag = false;
+    const bool OriginalFlag = true;
+    const bool DerivedFlag = false;
+    const bool ScheduledFlag = true;
+    const bool SkipFlag = false;
+    const bool PersistentFlag = true;
+    const bool TemporalFlag = false;
+    const bool ReasonFlag = true;
+    const bool RedundantFlag = false;
+
+    const bool FilePremise = OriginalFlag;
+    const bool FileProof = DerivedFlag;
+    const bool InstructionDeletion = InactiveFlag;
+    const bool InstructionIntroduction = ActiveFlag;
+
+	const int ArgumentsFilePremise = 1;
+	const int ArgumentsFileProof = 2;
+	const int ArgumentsRest = 3;
+	const int ArgumentsHelp = 4;
+	const int ArgumentsError = 5;
+}
+
+namespace Parameters {
+	extern std::string pathPremise;
+	extern std::string pathProof;
+	extern int bufferSize;
+	extern int databaseSize;
+	extern bool verbosity;
+	extern int hashDepth;
+
+	void setPremise(std::string path);
+	void setProof(std::string path);
+	void setVerbosity(bool value);
+}
+
+namespace Blablabla {
+	extern int level;
+
+	void increase();
+	void decrease();
+	void comment();
+	void log();
+}
+
+
+namespace Tools {
+	extern int commentLevel;
+
+	int compare (const void *a, const void *b);
+	void sortClause(int* cla, int length);
+	int leap(int x);
+
+	void comment(std::string str);
+	void increaseCommentLevel();
+	void decreaseCommentLevel();
+	std::string offsetToString(long offset);
+	std::string pointerToString(int* ptr);
+	std::string processingStackToString();
+	void resolutionChain();
+	void reprocessingStack();
+	void watchList(int literal);
+}
+
+#endif

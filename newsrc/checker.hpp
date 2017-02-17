5141c25e615747fb2cf98b6c71910905f93359d2
Adrian Rebola-Pardo <arpj.rebola@gmail.com>
2017-02-17 14:47:39 +0100

    a

---
+++ newsrc/checker.hpp
@@ -0,0 +1,15 @@
+#ifndef __CHECKER_H_INCLUDED__
+#define __CHECKER_H_INCLUDED__
+
+#include <stdlib.h>
+
+#include "database.hpp"
+#include "proof.hpp"
+#include "database.hpp"
+#include "watchlist.hpp"
+
+namespace Checker {
+    void restart();
+}
+
+#endif

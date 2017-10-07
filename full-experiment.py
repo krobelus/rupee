529aae25a685f8d23055668e2bf3eab91fde823a
Adrian Rebola-Pardo <arpj.rebola@gmail.com>
2017-10-07 13:14:26 +0200

    added cpu/disk/memory constraints to htcondor

--- full-experiment.py
+++ full-experiment.py
@@ -8,17 +8,19 @@ import shutil
 
 def generate(f, k):
     wr = open("jobs/" + str(k) + ".sub", "w")
-    wr.write("# Unix submit description file\n")
-    wr.write("\n")
-    wr.write("executable              = experiment.py\n")
-    wr.write("arguments               = " + f + " -delete \n")
-    # wr.write("output                  = " + f + ".condout\n")
-    # wr.write("errors                  = " + f + ".conderr\n")
-    wr.write("transfer_input_files    = " + f + ".cnf," + f + ".drat\n")
-    wr.write("transfer_output_files   = " + f + ".out," + f + ".DT.sick," + ".SD.sick," + f + ".FD.sick\n")
-    wr.write("should_transfer_files   = Yes\n")
-    wr.write("when_to_transfer_output = ON_EXIT\n")
-    wr.write("queue\n")
+    wr.write("""# Unix submit description file\n\n
+    request_cpus            = 1\n
+    request_memory          = 20G\n
+    request_disk            = 10G\n
+    executable              = experiment.py\n
+    arguments               = """ + f + """ -delete \n
+    output                  = """ + f + """.condout\n
+    errors                  = """ + f + """.conderr\n
+    transfer_input_files    = """ + f + ".cnf," + f + """.drat\n
+    transfer_output_files   = """ + f + ".out," + f + ".DT.sick," + ".SD.sick," + f + """.FD.sick\n
+    should_transfer_files   = Yes\n
+    when_to_transfer_output = ON_EXIT\n
+    queue\n""")
     wr.close()
 
 def clean():

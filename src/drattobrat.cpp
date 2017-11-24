9ba64aaa7ff6e5fdc3467f40e5532da998ff0fab
Adrian Rebola-Pardo <arpj.rebola@gmail.com>
2017-11-24 08:56:49 +0100

    deleted messages from drattobrat

--- src/drattobrat.cpp
+++ src/drattobrat.cpp
@@ -19,10 +19,8 @@ int main(int argc, char* argv[]) {
 		if(!inclause) {
 			if((parsed = fscanf(input, " d %i ", &number)) == 1) {
 				fputc(100, output);
-				std::cout << "print " << 100 << std::endl;
 			} else if((parsed = fscanf(input, " %i ", &number)) == 1) {
 				fputc(97, output);
-				std::cout << "print " << 97 << std::endl;
 			} else if(parsed == EOF) {
 				std::fclose(input);
 				std::fclose(output);
@@ -44,10 +42,8 @@ int main(int argc, char* argv[]) {
 				}
 			}
 		}
-		std::cout << "read " << number << std::endl;
 		if(number == 0) {
 			fputc(0, output);
-			std::cout << "print " << 0 << std::endl;
 			inclause = false;
 		} else {
 			if(number > 0) {
@@ -55,17 +51,13 @@ int main(int argc, char* argv[]) {
 			} else {
 				unumber = number * -2 + 1;
 			}
-			std::cout << "transform " << unumber <<std::endl;
 			do {
 				remainder = unumber & 127;
-				std::cout << "restrict " << remainder <<std::endl;
 				unumber = unumber >> 7;
 				if(unumber > 0) {
 					remainder |= 128;
 				}
-				std::cout << "rest " << unumber << std::endl;
 				fputc(remainder, output);
-				std::cout << "print " << remainder << std::endl;
 			} while(unumber > 0);
 		}
 	}
